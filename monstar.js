5eca5144b2e1e48f3d7a5135bf7fbe98eb393572
Raihanhossen <raihan.pust.cse@gmail.com>
2023-05-27 19:19:12 +0600

    first commit

---
+++ monstar.js
@@ -0,0 +1,92 @@
+const monstrs = [
+    {
+        id:1,
+        name:'Saiful Islam',
+        email:'saiful34@gmail.com',
+    },
+    {
+        id:2,
+        name:'Shihabul Islam',
+        email:'shihab@gmail.com',
+    },
+    {
+        id:3,
+        name:'Sujon Ali',
+        email:'sujon@gmail.com',
+    },
+    {
+        id:4,
+        name:'Rupom Islam',
+        email:'rupom@gmail.com',
+    },
+    {
+        id:5,
+        name:'Sifat Hasan',
+        email:'sifat@gmail.com',
+    },
+    {
+        id:6,
+        name:'kazi Muzahid',
+        email:'kazi@gmail.com',
+    },
+    {
+        id:7,
+        name:'Saiful Islam',
+        email:'saiful34@gmail.com',
+    },
+    {
+        id:9,
+        name:'Hasibul Islam',
+        email:'hasibul@gmail.com',
+    },
+    {
+        id:10,
+        name:'Sadikul Islam',
+        email:'sadik@gmail.com',
+    },
+    {
+        id:11,
+        name:'Badshah Bulbul',
+        email:'badshah@gmail.com',
+    },
+    {
+        id:12,
+        name:'Sakib Hossen',
+        email:'sakib@gmail.com',
+    },
+    {
+        id:13,
+        name:'Mehedi Hasan',
+        email:'mehedi@gmail.com',
+    },
+    {
+        id:14,
+        name:'Nahid Islam',
+        email:'nahid@gmail.com',
+    },
+    {
+        id:15,
+        name:'Sabbir Ahmed',
+        email:'sabbir@gmail.com',
+    },
+    {
+        id:16,
+        name:'Joh Shahriar',
+        email:'joy@gmail.com',
+    },
+    {
+        id:17,
+        name:'Huraira Islam',
+        email:'abuhuraira@gmail.com',
+    },
+    {
+        id:18,
+        name:'Riad Islam',
+        email:'riad@gmail.com',
+    },
+    {
+        id:19,
+        name:'Pikul Hossen',
+        email:'pikul@gmail.com',
+    },
+]
